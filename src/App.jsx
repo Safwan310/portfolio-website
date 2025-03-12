247eea7beeeec0bd75fd694191b81f13970a9f08
Safwan310 <mdsafwan3102001@gmail.com>
2025-03-12 23:22:36 +0530

    Initial content and styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import NotFound from './NotFound.jsx';
 function App() {
   return (
     <>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Welcome!</title>
+    </head>
+    <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} /> {/* 404 Page */}
       </Routes>
+    </div>
     </>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1 class="text-3xl font-bold underline">Home Page</h1>;
+    return (
+        <div className="h-screen flex justify-center">
+            <div className="h-full flex flex-col justify-center items-center">
+                <div>
+                    
+                </div>
+                <div className="flex flex-col justify-center items-center w-2/3">
+                    <h1>Mohammed Safwan S - Fullstack developer</h1>
+                    <p>Hi there! 👋🏼 Welcome to my site. I'm Safwan, a Full Stack Developer at Zoho Corporation, Chennai. My interests include problem-solving, exploring modern web technologies, and reading. In my spare time, I enjoy reading articles like <a className="text-blue-500" href="https://discord.com/blog/how-discord-stores-billions-of-messages">this one</a> as I find engineering at scale fascinating.</p>
+                </div>
+            </div>
+        </div>
+    )
   }
   
   export default Home;
